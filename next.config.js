de0e490d921291360d8c2cf7075850a55a5d0e7b
hiramatsutaku <hiramatsutaku@gmail.com>
2019-12-21 15:01:39 +0900

    Add CONTENTFUL_* to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
+});
