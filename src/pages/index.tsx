a662e84f96e43eba07bfcc7869b1b43012890c2c
hiramatsutaku <hiramatsutaku@gmail.com>
2019-12-21 15:04:31 +0900

    Render posts from contentful on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
-const Index = () => (
-  <div>
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import { createClient, Entry } from 'contentful';
+import { NextPage } from 'next';
+import { PostFields } from '../domain/entities/PostEntity';
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
+const POST_CONTENT_TYPE = 'post';
+
+interface Props {
+  items: Entry<PostFields>[];
+}
+
+const Index: NextPage<Props> = ({ items }) => (
+  <Layout>
     <p>Hello Next.js</p>
-  </div>
+    {items.map(({ fields }) => (
+      <li key={fields.title}>
+        <Link href={`/p/[postId]`} as={`/p/${fields.title}`}>
+          <a>{fields.title}</a>
+        </Link>
+      </li>
+    ))}
+  </Layout>
 );
+
+Index.getInitialProps = async (): Promise<Props> => {
+  const entries = await client.getEntries<PostFields>({
+    content_type: POST_CONTENT_TYPE,
+  });
+  return {
+    items: entries.items,
+  };
+};
 
 export default Index;
