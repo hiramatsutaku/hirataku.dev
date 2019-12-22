8d8f3ecc9b88cb5d9a7c30c6819f3137644c80f6
hiramatsutaku <hiramatsutaku@gmail.com>
2019-12-22 14:01:54 +0900

    Use contentful preview api on staging
    Fix typo

--- src/domain/repositories/contentful.ts
+++ src/domain/repositories/contentful.ts
@@ -1,6 +1,12 @@
 import { createClient } from 'contentful';
 
-export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
-});
+export const client = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN!
+  ? createClient({
+      host: 'preview.contentful.com',
+      space: process.env.CONTENTFUL_SPACE_ID!,
+      accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN!,
+    })
+  : createClient({
+      space: process.env.CONTENTFUL_SPACE_ID!,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+    });
