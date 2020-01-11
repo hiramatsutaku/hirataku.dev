f83953f5c740657ee05d9af00c107d26df4ae392
hiramatsutaku <hiramatsutaku@gmail.com>
2020-01-11 21:21:14 +0900

    Fix post component

--- src/presentation/components/Post.tsx
+++ src/presentation/components/Post.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import IPostEntity from '../../domain/post/PostEntity';
 
-const Wrapper = styled.header``;
+const Wrapper = styled.div`
+  padding: 10px;
+`;
 
 export const Post: React.FC<{ post: IPostEntity }> = ({ post }) => (
   <Wrapper>
