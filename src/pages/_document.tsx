21fb2267cbd89b3e43cb42dd7aea2664c37b3565
hiramatsutaku <hiramatsutaku@gmail.com>
2020-01-11 21:19:03 +0900

    Add lang property to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import Document, { DocumentInitialProps, DocumentContext } from 'next/document';
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,5 +31,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
