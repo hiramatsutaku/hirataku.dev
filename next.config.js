6965a7c2a5ac5c3f23f78eacb12e9ff989838dd1
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-12 17:58:54 +0900

    Configure @next/mdx

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withMDX = require('@next/mdx')();
 
-module.exports = withBundleAnalyzer({
-  env: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-    CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-  },
-});
+module.exports = withBundleAnalyzer(
+  withMDX({
+    env: {
+      CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+      CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+      CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    },
+    pageExtensions: ['mdx'],
+  }),
+);
