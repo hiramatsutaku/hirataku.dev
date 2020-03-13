e810e51e6af750371cbf1e555720fca93b3ebf9f
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-14 03:41:03 +0900

    Fix test name

--- src/domain/post/PostEntity.test.ts
+++ src/domain/post/PostEntity.test.ts
@@ -1,7 +1,7 @@
 import { PostEntity } from './PostEntity';
 
-describe('create instance', () => {
-  test('success', () => {
+describe('PostEntity', () => {
+  test('create instance', () => {
     const slug = 'hogehoge';
     const title = 'title';
     const instance = new PostEntity({ slug, title });
