acf0c381fa50b20e90692b9f967c51929d193c04
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-15 08:12:30 +0900

    Remove duplicated title

--- src/presentation/components/Post.tsx
+++ src/presentation/components/Post.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import dynamic from 'next/dynamic';
 import { PostEntity } from '../../domain/post/PostEntity';
-import { Title } from './Title';
 
 const Wrapper = styled.div`
   padding: 0;
@@ -12,7 +11,6 @@ export const Post: React.FC<{ post: PostEntity }> = ({ post }) => {
   const MdxComponent = dynamic(() => import(`../../posts/${post.slug}`));
   return (
     <Wrapper>
-      <Title text={post.title} />
       <MdxComponent />
     </Wrapper>
   );
