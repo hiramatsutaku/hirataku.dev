632720ecfcb6f4ca5770c74865e16a82dbdeff71
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-14 20:32:38 +0900

    Configure jest-next-dynamic

--- src/presentation/components/Post.stories.tsx
+++ src/presentation/components/Post.stories.tsx
@@ -1,6 +1,11 @@
+import preloadAll from 'jest-next-dynamic';
 import React from 'react';
 import { Post } from './Post';
 import { PostEntity } from '../../domain/post/PostEntity';
+
+beforeAll(async () => {
+  await preloadAll();
+});
 
 const post = new PostEntity({ slug: 'hogehoge', title: 'タイトル' });
 
