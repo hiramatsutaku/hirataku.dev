78ca684563b75a7f497e9a7dca41adf0f42da75f
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-17 13:05:00 +0900

    Add CodeBlock component

---
+++ src/presentation/components/CodeBlock.tsx
@@ -0,0 +1,21 @@
+import React, { HTMLAttributes } from 'react';
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+
+export const CodeBlock: React.FC<HTMLAttributes<HTMLDivElement>> = ({ children, className }) => {
+  const language = className?.replace(/language-/, '');
+  return (
+    <Highlight {...defaultProps} code={children as string} language={language as Language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
