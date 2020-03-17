fe0b0c397b66b530c3c86bc7c525c2c977d3d955
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-18 05:27:46 +0900

    Update style

--- src/presentation/components/CodeBlock.tsx
+++ src/presentation/components/CodeBlock.tsx
@@ -6,7 +6,7 @@ export const CodeBlock: React.FC<HTMLAttributes<HTMLDivElement>> = ({ children, 
   return (
     <Highlight {...defaultProps} code={children as string} language={language as Language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
+        <pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
